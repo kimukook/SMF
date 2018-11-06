2350ff9debf5ec74af2bdea4025b20686c6da9c5
kimukook <muhan_zhao1994@hotmail.com>
2018-11-05 16:54:32 -0800

    Hookes GPS fixed. And kriging fixed most part.

--- Kriging.py
+++ Kriging.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from itertools import combinations
 
 def test_fun_schwefel(n):
     lb = np.zeros(n)
@@ -23,16 +23,7 @@ for i in range(m):
     y[i] = fun(x[:, i].reshape(-1, 1))
 
 
-def normalize_input(x):
-    '''
-
-    :param x: 2-dimensional array
-    :return: normalized array x by (x - mean x ) / std(x)
-    '''
-    return
-
-
-def main_dace(y, x, regr, corr, theta0):
+def main_dace(y, x, regr_keyword, corr_keyword, theta0):
     '''
 
     :param y: The function values y_i at the sites x_i
@@ -50,23 +41,150 @@ def main_dace(y, x, regr, corr, theta0):
     # Normalize data, x & y --> X & Y
     Y = (y - np.mean(y)) / (np.std(y))
     X = (x - np.mean(x, axis=1).reshape(-1, 1)) / np.std(x, axis=1).reshape(-1, 1)
-    F = evaluate_regression(regr, X)
+
+    regr = eval_regression_basis(regr_keyword)
+    F = regr(x)
+    corr_scalr, corr_vector = correlation_basis(corr_keyword)
     psi = minimize_psi(theta0, corr, Y, X, F)
     return psi
 
 
-def evaluate_regression(regr, X):
-    n, m = X.shape
-    F = np.zeros((m, m))
+def eval_regression_basis(keyword):
+    '''
+
+    :param keyword: should be linear or quadratic
+    :return: a basis function
+    '''
+    assert isinstance(keyword, np.str), 'The input name "keyword" should be a string, linear or quadratic'
+    if keyword =='linear':
+        def linear_basis(x):
+            '''
+            Return a basis regression, consists of n+1 functions. 1 constant function plus each coordinates.
+            :param x: The sites x.
+            :return: n+1 basis functions.
+            '''
+            assert isinstance(x, np.ndarray), 'The input of sites matrix x is not an array!'
+            n, m = x.shape
+            F = np.hstack((np.ones((m, 1)), x.T))
+            return F
+        return linear_basis
+
+    elif keyword == 'quadratic':
+        def quadratic_basis(x):
+            '''
+            Return a basis regression, consists of (n+1)(n+2)/2. 1 constant function, n linear basis functions,
+            n square quadratic, (n+1)n/2 cross-term basis functions.
+            :param x: The sites x
+            :return: (n+1)(n+2)/2 basis functions.
+            '''
+            assert isinstance(x, np.ndarray), 'The input of sites matrix x is not an array!'
+            n, m = x.shape
+
+            # linear part
+            F = np.hstack((np.ones((m, 1)), x.T))
+
+            # square terms part
+            F = np.hstack(( F, x.T**2 ))
+
+            # Cross-terms part
+            nlist = np.arange(1, n + 1)
+            # the order of functions in F can be shuffled.
+            comb = np.array([list(c) for c in combinations(nlist, 2)])  # we use 2 because its quadratic.
+            comb_matrix = x[comb]
+            cross_term = comb_matrix[:, 0, :] * comb_matrix[:, 1, :]
+            F = np.hstack((F, cross_term.T))
+            return F
+        return quadratic_basis
+
+    else:
+        print('Keyword should be "linear" or "quadratic" temporarily ')
+
+
+def correlation_basis(keyword):
+    '''
+    Return the correlation function of two sites.
+    :param keyword: Determine the type of correlation.
+    :return:  A function that evaluates the correlation between two sites.
+    '''
+    if keyword == 'correxp':
+        def correxp_scalar(theta, x, y):
+            '''
+            Evaluate the exponential correlation between two sites.
+            :param theta: Weight parameters for different dimensions, should be a 1 by n vector for now.
+            :param x: One site, has the form as n by , vector.
+            :param y: Another site, has the form as n by , vector.
+            :return: An exponential correlation function
+            '''
+            diff = np.abs(x-y)
+            return np.exp(-theta.dot(diff))
+
+
+        def correxp_vector(theta, x, Y):
+            '''
+            Evaluate the exponential correlation between site x and other sites Y.
+            :param theta: Weight parameters for different dimensions, should be a 1 by n vector for now.
+            :param x: One site, has the form as n by , vector.
+            :param y: Another site, has the form as n by m vector.
+            :return: An exponential correlation function
+            '''
+            theta = theta.reshape(-1, 1).T  # transform theta to be a 2D, n by 1 vector.
+            x = x.reshape(-1, 1)  # transform x into a 2D, n by 1 vector
+            diff = np.abs(Y - x)
+            return np.exp(-theta.dot(diff))
+        return correxp_scalar, correxp_vector
+
+    elif keyword == 'Gaussian':
+        def corrgauss_scalar(theta, x, y):
+            '''
+            Evaluate the exponential correlation between two sites.
+            :param theta: Weight parameters for different dimensions, should be a 1 by n vector for now.
+            :param x: One site, has the form as n by , vector.
+            :param y: Another site, has the form as n by , vector.
+            :return: An gaussian correlation function
+            '''
+            return np.exp(-theta.dot((x-y)**2))
+
+
+        def corrgauss_vector(theta, x, Y):
+            '''
+            Evaluate the exponential correlation between site x and other sites Y.
+            :param theta: Weight parameters for different dimensions, should be a 1 by n vector for now.
+            :param x: One site, has the form as n by , vector.
+            :param y: Another site, has the form as n by m vector.
+            :return: An exponential correlation function
+            '''
+            theta = theta.reshape(-1, 1).T  # transform theta to be a 2D, n by 1 vector.
+            x = x.reshape(-1, 1)  # transform x into a 2D, n by 1 vector
+            diff = np.abs(Y - x)
+            return np.exp(-theta.dot(diff**2))
+        return corrgauss_scalar, corrgauss_vector
+
+    else:
+        print('Wrong type of keyword!')
+
+
+def corr_eval(corr_vector, theta, x):
+    assert isinstance(x, np.ndarray), 'The input of sites matrix x is not an array!'
+    assert x.ndim == 2, 'The input of sites matrix x should be 2 dimension array!'
+    n, m = x.shape
+    Phi = np.zeros((m, m))
     for i in range(m):
-        for j in range(m):
+        Phi[i, :] = corr_vector(theta, x[:, i], x)
+    return Phi
 
 
-    return F
 
-
-def minimize_psi(theta0, corr, Y, X, F):
-
+def psi_eval(theta, corr, y, x, F):
+    n, m = x.shape
+    Phi = corr_eval(corr, theta, x)  # TODO need add a mu*I on the digonal.
+    C = np.linalg.cholesky(Phi)
+    C_inv = np.linalg.inv(C)  # TODO This could be optimized by forward substitution.
+    tilde_F = np.dot(C_inv, F)
+    tilde_Y = np.dot(C_inv, y)
+    Q, R = np.linalg.qr(tilde_F)
+    beta = np.dot(np.dot(np.linalg.inv(R), Q.T), tilde_Y)
+    residual = tilde_Y - np.dot(tilde_F, beta)
+    sigma2 = 1/m * np.dot(residual.T, residual)
+    psi = np.linalg.det(Phi) ** (1/m) * sigma2
     return psi
-
 
---
+++ Modified_Hookes_GPS.py
@@ -0,0 +1,140 @@
+import numpy as np
+''' 
+This is a scripy that implemented Modified Hookes and Jeeves Generalized Pattern Search Algorithm.
+For more information, please refer "Introduction to Optimziation and Data Fitting" by K. Madsen, 2008. Section 4.3.
+'''
+
+
+def explore(x, delta, func):
+    '''
+    Implement the Algorithm 4.2: Explore around the current best point by evaluating points with the unit length distance
+    in each coordiante.
+    :param x:       The current best point. Form: n by 1 vector.
+    :param delta:   The step length of current move.
+    :param func:    The objective function that aims at minimizing.
+    :return: A new point for exploration.
+    '''
+    n = x.shape[0]
+    x_bar = np.copy(x)
+    for i in range(n):
+        new_x = step_length_constraints_check(delta, x_bar, i)
+        psi_value = np.zeros(3)
+        for j in range(new_x.shape[1]):
+            psi_value[j] = func(new_x[:, j].reshape(-1, 1))
+        index = np.argmin(psi_value)
+        x_bar += (index - 1) * np.abs((new_x[:, index].reshape(-1, 1) - x_bar))
+    return x_bar
+
+
+def step_length_constraints_check(delta, x_hat, i):
+    '''
+    Boudn the coordinate seach inside the box domain.
+    :param delta: Step length.
+    :param x_hat: current best point
+    :param i: Coordinate
+    :return: Bounded step points in coordinate i.
+    '''
+    n = x_hat.shape[0]
+    step = delta * np.identity(n)[:, i].reshape(-1, 1)
+    new_x = np.hstack((x_hat - step, x_hat, x_hat + step))
+    for i in range(new_x.shape[0]):
+        for j in range(new_x.shape[1]):
+            if new_x[i, j] > 1:
+                new_x[i, j] = 1
+            elif new_x[i, j] < 0:
+                new_x[i, j] = 0
+    return new_x
+
+
+def bound_base_point(z):
+    '''
+    Bound the base point in the box domain.
+    :param z:
+    :return: Bounded base point.
+    '''
+    n = z.shape[0]
+    for i in range(z.shape[0]):
+        for j in range(z.shape[1]):
+            if z[i, j] > 1:
+                z[i, j] = 1
+            elif z[i, j] < 0:
+                z[i, j] = 0
+    return z
+
+
+def check_stop(delta, delta_threshold, iter_max, iter_num):
+    '''
+    Check whether or not the stop criteria is satisfied.
+    :param delta: step length
+    :param delta_threshold: target step length
+    :param iter_max: Maximum number of iterations
+    :param iter_num: Current iteration number
+    :return: Stop or not.
+    '''
+    if delta <= delta_threshold or iter_num >= iter_max:
+        stop = 1
+    else:
+        stop = 0
+    return stop
+
+
+def move(x, delta, func, iter_max, delta_refine):
+    '''
+    The main script.
+    :param x: Current best point or initial point.
+    :param delta: Initial step length.
+    :param func: Objective function.
+    :param iter_max: Maximum number of function evaluations
+    :param delta_refine: Times of refinement on delta.
+    :return: Minimizer.
+    '''
+    assert x.ndim == 2, 'The input x should be n by 1 vector!'
+    assert isinstance(x, np.ndarray), 'The inputx is not a np.ndarray!'
+    delta_threshold = (1/2) ** delta_refine * delta
+    x_hat = explore(x, delta, func)
+    stop = 0
+    iter_num = 0
+    while not stop:
+        iter_num += 1
+        if func(x_hat) < func(x):
+            z = np.copy(x_hat + (x_hat - x))
+            z = bound_base_point(z)
+            x = np.copy(x_hat)
+        else:
+            z = np.copy(x)
+            delta /= 2
+        x_hat = explore(z, delta, func)
+        stop = check_stop(delta, delta_threshold, iter_max, iter_num)
+        print('xhat = ', x_hat.T)
+        print('delta = ', delta)
+    return x_hat, iter_num, delta
+
+
+# # Test case:
+# def test_fun_schwefel(n):
+#     lb = np.zeros(n)
+#     ub = np.ones(n)
+#     fun = lambda x: - sum(np.multiply(500 * x, np.sin(np.sqrt(abs(500 * x))))) / 250
+#     y0 = -1.6759316 * n  # targert value for objective function
+#     xmin = 0.8419 * np.ones((n, 1))
+#     fname = 'Schewfel'
+#     return fname, xmin, y0, fun, lb, ub
+#
+# n = 2
+# m = 10
+# Nm = 8
+# x = np.random.rand(n, m)
+# x = np.round(x * Nm) / Nm
+# y = np.zeros(m)
+# fname, xmin, y0, fun, lb, ub = test_fun_schwefel(n)
+#
+# for i in range(m):
+#     y[i] = fun(x[:, i].reshape(-1, 1))
+#
+# best = x[:, np.argmin(y)].reshape(-1, 1)
+# delta = 0.2
+# iter_max = 100
+# delta_refine = 8
+#
+#
+# x_minima, iter_num, delta = move(best, delta, fun, iter_max, delta_refine)
